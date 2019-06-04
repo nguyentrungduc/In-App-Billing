7e9e607e002342c7f0c84904c49e3333f1d92125
Admin <nguyenduc049@gmail.com>
2019-06-04 09:13:54 +0700

    add

--- app/src/main/java/com/sun/ntduc/iab/CachedPurchase.kt
+++ app/src/main/java/com/sun/ntduc/iab/CachedPurchase.kt
@@ -1,8 +1,6 @@
 package com.sun.ntduc.iab
 
 import androidx.room.*
-import com.android.billingclient.api.Purchase
-import com.google.gson.Gson
 
 @Entity(tableName = "purchase_table")
 @TypeConverters(PurchaseTypeCoverter::class)
